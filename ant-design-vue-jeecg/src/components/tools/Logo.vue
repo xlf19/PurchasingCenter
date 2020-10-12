5cddc5efd4ec06667cad9c3848d01ac5c5a31495
xlf19 <2107595528@qq.com>
2020-10-12 13:14:52 +0800

    xlf

--- ant-design-vue-jeecg/src/components/tools/Logo.vue
+++ ant-design-vue-jeecg/src/components/tools/Logo.vue
@@ -21,7 +21,7 @@
     props: {
       title: {
         type: String,
-        default: 'Jeecg-Boot Pro',
+        default: 'PMP',
         required: false
       },
       showTitle: {
--- ant-design-vue-jeecg/src/views/dashboard/Analysis.vue
+++ ant-design-vue-jeecg/src/views/dashboard/Analysis.vue
@@ -7,8 +7,8 @@
       请选择首页样式：
       <a-radio-group v-model="indexStyle">
         <a-radio :value="1">统计图表</a-radio>
-        <a-radio :value="2">统计图表2</a-radio>
-        <a-radio :value="3">任务表格</a-radio>
+        <!-- <a-radio :value="2">统计图表2</a-radio>
+        <a-radio :value="3">任务表格</a-radio> -->
       </a-radio-group>
     </div>
   </div>
