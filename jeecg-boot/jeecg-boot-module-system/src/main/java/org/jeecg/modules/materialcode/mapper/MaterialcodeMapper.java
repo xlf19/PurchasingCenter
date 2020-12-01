c5638d7ad1881d3bacd8958611d578d9a60fd38d
unknown <1246984093@qq.com>
2020-12-01 09:57:31 +0800

    新增物资编码维护

---
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/materialcode/mapper/MaterialcodeMapper.java
@@ -0,0 +1,17 @@
+package org.jeecg.modules.materialcode.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.jeecg.modules.materialcode.entity.Materialcode;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @Description: 物资编码表
+ * @Author: jeecg-boot
+ * @Date:   2020-11-30
+ * @Version: V1.0
+ */
+public interface MaterialcodeMapper extends BaseMapper<Materialcode> {
+
+}
---
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/materialcode/service/IMaterialcodeService.java
@@ -0,0 +1,14 @@
+package org.jeecg.modules.materialcode.service;
+
+import org.jeecg.modules.materialcode.entity.Materialcode;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * @Description: 物资编码表
+ * @Author: jeecg-boot
+ * @Date:   2020-11-30
+ * @Version: V1.0
+ */
+public interface IMaterialcodeService extends IService<Materialcode> {
+
+}
